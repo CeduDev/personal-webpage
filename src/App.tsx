e7250bf8ccc102069501634a8c41c3a40b548d56
CeduDev <cedric.neergaard@gmail.com>
2021-12-20 11:38:36 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const App = () => (
+  <Container>
+    <h1 style={{ textAlign: 'center' }}>My Webpage!</h1>
+  </Container>
+);
+
+export default App;
