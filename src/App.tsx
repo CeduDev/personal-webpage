60ae3462b3480a697ab7b69eb011943b2d7f7eb4
CeduDev <cedric.neergaard@gmail.com>
2021-12-22 17:35:17 +0200

    made a temporary website (neon button), will add more to it later on

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import NavBar from './components/NavBar/NavBar';
 import LandingPage from './components/LandingPage/LandingPage';
-import FirstTimeline from './components/FirstTimeline/FirstTimeline';
 
-const App = () => (
-  <Container
-    style={{
-      padding: 0,
-      margin: 0,
-      width: '100vw',
-      maxWidth: '100%',
-    }}
-  >
-    <LandingPage />
-    <FirstTimeline />
-    <NavBar />
-  </Container>
-);
+const App = () => <LandingPage />;
 
 export default App;
